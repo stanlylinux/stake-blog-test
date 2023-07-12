daa4bbfa552e0186f9b2906d705c71c458b29ed8
stanlys96 <stanlyskwok@gmail.com>
2023-07-12 17:05:28 +0700

    walao

--- src/app/components/MainArticle.tsx
+++ src/app/components/MainArticle.tsx
@@ -40,15 +40,21 @@ export const MainArticle = ({
       className={`grid text-center md:text-left gap-y-3 md:grid-cols-5 gap-x-6 cursor-pointer article-container`}
     >
       <img
-        className="thumbnail md:col-span-3 pt-[0.9em] article-img ease-in-out duration-300"
+        className="thumbnail md:col-span-3 pt-[0.9em] article-img ease-in-out duration-300 w-full"
         src={imageUrl}
         alt="No Image"
       />
       <div className="mt-[15px] md:col-span-2">
         <p className="font-bold text-lg">{title}</p>
-        <p className="mt-[10px] text-lightGray text-sm lightGray-important">
-          {description}
-        </p>
+        <div
+          className="mt-[10px] text-lightGray text-sm lightGray-important"
+          dangerouslySetInnerHTML={{
+            __html:
+              description.trim().length > 100
+                ? description.trim().slice(0, 100) + "..."
+                : description,
+          }}
+        />
       </div>
     </div>
   );
